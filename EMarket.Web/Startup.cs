ee6a8ae2a17551f93d33d072e1fea5e6a307780d
cansundemirel <cansundemirel@gmail.com>
2019-12-13 18:30:31 +0300

    Kategori servis

--- EMarket.Web/Startup.cs
+++ EMarket.Web/Startup.cs
@@ -14,6 +14,8 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using EMarket.Infrastructure.Data;
 using EMarket.ApplicationCore.Entities;
+using EMarket.ApplicationCore.Interfaces;
+using EMarket.ApplicationCore.Services;
 
 namespace EMarket.Web
 {
@@ -41,7 +43,13 @@ namespace EMarket.Web
                     Configuration.GetConnectionString("DefaultConnection")));
             services.AddIdentity<ApplicationUser, IdentityRole>()
                 .AddDefaultUI(UIFramework.Bootstrap4)
-                .AddEntityFrameworkStores<ApplicationDbContext>();
+                .AddEntityFrameworkStores<ApplicationDbContext>()
+                .AddDefaultTokenProviders();
+
+            services.AddScoped(typeof(IRepository<>), typeof(EfRepository<>));
+            services.AddScoped<ICategoryService, CategoryService>();
+
+
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
         }
